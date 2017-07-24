d4e523f581f4d27a60691e2fe01fcdef75207c5b
Alessandro Aglietti <alessandro.aglietti@gmail.com>
2017-07-24 13:33:58 +0200

    travis

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # phpunit-mock-objects-twice-callback-issue
+
+
+same problem descripted here
+
+
+https://github.com/sebastianbergmann/phpunit-mock-objects/issues/181
+
+
+but when testing mocks that are using stream things going badly then 'called twice'
+
+
+UploadSimulator had 2 methods
+- upload (string, string, stream)
+- upload_simplified(string)
+
+upload_simulator generate resources name and open the stream, then pass all to upload
+
+
+TestCase simulate two consecutive uploads using
+- withConsecutive
+- callback
+
+as the Travis log show the callbacks are invoked more tha expected and in the `test_upload_simplified_two_consecutive_uploads` is used the FIRST callback instead of the SECOND callback
--- tests/phpunit/aqquadro/UploadSimulatorTest.php
+++ tests/phpunit/aqquadro/UploadSimulatorTest.php
@@ -54,6 +54,45 @@ class UploadSimulatorTest extends PHPUnit\Framework\TestCase
                         $this->assertFileEquals($test_resource, stream_get_meta_data($fp)['uri']);
                         return true;
                     })
+                ]
+            )
+            ->willReturn(true);
+
+        $uploader_result = $uploader_mock->upload_simplified($test_resource_id);
+        $this->assertTrue($uploader_result);
+
+        $this->assertEquals(1, $calls_count, 'callback function calls more than expected');
+    }
+
+    public function test_upload_simplified_two_consecutive_uploads()
+    {
+        $this->assertEquals(42, 42);
+        $test_resource = self::$TEST_RESOURCE_ID_PATH;
+        $test_resource_id = self::$TEST_RESOURCE_ID;
+
+        /**
+         * @var $uploader_mock \aqquadro\UploadSimulator
+         */
+        $uploader_mock = $this->getMockBuilder(\aqquadro\UploadSimulator::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['upload'])
+            ->getMock();
+
+        $calls_count = 0;
+
+        $uploader_mock->expects($this->exactly(2))
+            ->method('upload')
+            ->withConsecutive(
+                [
+                    'gold_bucket',
+                    "cdn/${test_resource_id}",
+                    $this->callback(function ($fp) use ($test_resource, &$calls_count) {
+                        $calls_count++;
+                        $this->assertEquals('resource', gettype($fp), "FIRST on callcount n.${calls_count}");
+                        $this->assertNotEquals('Unknown', get_resource_type($fp), "FIRST on callcount n.${calls_count}");
+                        $this->assertFileEquals($test_resource, stream_get_meta_data($fp)['uri']);
+                        return true;
+                    })
                 ],
                 [
                     'gold_bucket',
@@ -75,6 +114,6 @@ class UploadSimulatorTest extends PHPUnit\Framework\TestCase
         $uploader_result = $uploader_mock->upload_simplified($test_resource_id);
         $this->assertTrue($uploader_result);
 
-        $this->assertEquals(2, $calls_count);
+        $this->assertEquals(2, $calls_count, 'callback function calls more than expected');
     }
 }
